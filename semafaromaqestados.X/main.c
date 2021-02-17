c50496c33416b1b5e09e376a69fe582677ba6de4
20185092 <20185092@DSN1271158624.cfp127aluno.local>
2021-02-17 15:24:39 -0200

    projectnumber1

---
+++ semafaromaqestados.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: 20185092
+ *
+ * Created on 17 de Fevereiro de 2021, 14:56
+ */
+
+
+#include <xc.h>
+#include "config.h"
+
+void main(void) 
+{
+    while(1)
+    {
+        
+    }
+}
+    
